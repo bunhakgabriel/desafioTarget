affcf8109252ce3fa2e7d0e318fc3cf8f474e521
Gabriel Bunhak <bunhakgabriel@gmail.com>
2024-04-09 01:02:31 -0300

    ex 4

---
+++ exercicio4.js
@@ -0,0 +1,20 @@
+/*
+
+4) Eu ligaria o primeiro interruptor e o deixaria ligado, o segundo interruptor 
+eu o ligaria e o deixaria ligado por alguns minutos e depois o desligaria, e o 
+terceiro interruptor eu não mexeria, ele ficaria desligado. Como eu tenho duas 
+idas até as salas, na primeira ida a uma das salas eu verificaria a lâmpada, 
+se a lâmpada estiver ligada ela corresponde ao primeiro interruptor que eu deixei 
+ligado, se a lâmpada estiver desligada mais um pouco quente significa que ela é 
+do segundo interruptor que eu liguei por alguns minutos e depois desliguei, e se 
+a lâmpada estiver desligada e fria significa que ela corresponde ao último 
+interruptor que não foi ligado. Vamos assumir que nessa primeira sala que eu 
+escolhi a lâmpada esteja desligada e quente, então ela corresponde ao segundo 
+interruptor que foi ligado e posteriormente desligado, me restou apenas mais uma 
+ida a alguma sala, digamos que nessa outra sala a lâmpada esteja desligada e fria, 
+então ela corresponde ao último interruptor que não foi ligado, já não me restam 
+mais nenhuma ida a nenhuma sala, porém só restou uma sala e um interruptor, 
+que foi o interruptor que ficou ligado, então concerteza nessa última sala a 
+lâmpada estará ligada, e ela corresponde ao primeiro interruptor que ficou ligado.
+
+*/
